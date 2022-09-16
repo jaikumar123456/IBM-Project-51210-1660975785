5036abf21e535d3e3dfce00f0ea366133506daaf
PRANAVA RAMAN B M S <bmspr1502@gmail.com>
2022-09-16 15:23:11 +0530

    Create file1.py

---
+++ Pranava_Raman_BMS/Assignment1/file1.py
@@ -0,0 +1,23 @@
+#Creates a list
+list1 = [1,2,3]
+
+#insert int at a position
+list1[0] = 0
+
+#Delete the first occurance
+list1.pop(0)
+
+#Insert at the end
+list1.append(4)
+print(list1)
+
+#Sort the list
+list1.sort()
+print(list1)
+
+#pop the last element
+list1.pop()
+
+#Reverse the list
+list1.reverse()
+print(list1)
